7c34709121b068d1cefde746a542ce729eeb15ce
Ruzzle Wayne Nuer <nuerruzzle@gmail.com>
2024-04-27 22:34:03 +0800

    finished header layout

---
+++ script.js
@@ -0,0 +1,17 @@
+/**
+ * Website
+ *
+ * Navbar - name | Home, skills, Portfolio, Contact, About | Hire me(button)
+ *
+ * Introduction, Hi I'm Ruzzle, Aspiring Full Stack Web developer(stuff about me)
+ *
+ * Skills - pretty self explanatory
+ *
+ * Portfolio - Projects that we made (make them worth looking)
+ *
+ * Thank them for their consideration and time
+ *
+ * Contact | About - emails and whatnot
+ *
+ *
+ */
